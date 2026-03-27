ca6908172fb3c30914288716811711cfefce9fd5
FantomuSheru <alanpicazzopartida@gmail.com>
2026-03-26 23:51:45 -0600

    Update brent.js

--- brent.js
+++ brent.js
@@ -1,5 +1,25 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    brent: 80.00
-  });
+
+  // 🔥 SOLUCIÓN CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
+  try {
+    const response = await fetch(
+      "https://query1.finance.yahoo.com/v8/finance/chart/BZ=F"
+    );
+
+    const data = await response.json();
+
+    const precio = data.chart.result[0].meta.regularMarketPrice;
+
+    return res.status(200).json({
+      brent: precio
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      error: "Error obteniendo Brent"
+    });
+  }
 }
