3e421753ce900fd87b9a62833f25c136070dc8d0
FantomuSheru <alanpicazzopartida@gmail.com>
2026-03-26 23:39:11 -0600

    brent.js

--- brent.js
+++ brent.js
@@ -1,20 +1,5 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      "https://query1.finance.yahoo.com/v8/finance/chart/BZ=F"
-    );
-
-    const data = await response.json();
-
-    const precio = data.chart.result[0].meta.regularMarketPrice;
-
-    return res.status(200).json({
-      brent: precio
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      error: "Error obteniendo Brent"
-    });
-  }
+  return res.status(200).json({
+    brent: 80.00
+  });
 }
